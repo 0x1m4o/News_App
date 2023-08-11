f81e670790cd198d6115754169419a96ca28b78b
0x1m40 <chuntdevnx@gmail.com>
2023-08-11 17:34:49 +0700

    Update repository implementation to using the datasource

--- lib/features/news/data/repository-impl/article_repository_impl.dart
+++ lib/features/news/data/repository-impl/article_repository_impl.dart
@@ -1,10 +1,36 @@
-import 'package:news_app/features/news/data/models/article_model.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first, depend_on_referenced_packages
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app/features/news/data/models/article_model.dart';
+import 'package:dio/dio.dart';
 import 'package:news_app/features/news/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService newsApiService;
+  ArticleRepositoryImpl({
+    required this.newsApiService,
+  });
   @override
-  Future<DataState<List<ArticleModel>>> getAllNews() {
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getAllNews(
+      {String? country, String? category}) async {
+    try {
+      final httpResponse = await newsApiService.getNewsArticle(
+          apiKey: dotenv.env['apiKey'], country: country, category: category);
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioException(
+            requestOptions: httpResponse.response.requestOptions,
+            response: httpResponse.response,
+            error: httpResponse.response.statusMessage,
+            type: DioExceptionType.badResponse));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
